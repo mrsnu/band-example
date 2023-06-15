dc5b576c04e09ba02f614cb134cc1114bde43062
Jingyu Lee <dostos10@gmail.com>
2023-06-15 19:12:54 +0900

    Remove files from submodules

--- unreal_engine/Plugins/AndroidCamera/Source/AndroidCamera/Public/AndroidCameraPermission.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-void check_android_permission(const char* permission);
--- unreal_engine/Plugins/AndroidCamera/Source/VideoInput/Public/VideoInputEnum.h
+++
@@ -1,9 +0,0 @@
-﻿#pragma once
-
-UENUM(BlueprintType)
-enum class EVideoStatus : uint8 {
-  NOT_INITIALIZED UMETA(DisplayName = "NotInitialized"),
-  NOT_STARTED UMETA(DisplayName = "NotStarted"),
-  PLAYING UMETA(DisplayName = "Playing"),
-  FINISHED UMETA(DisplayName = "Finished"),
-};
