7322d72d164bc547687740131a3f02c7ba2b4ea2
Jingyu Lee <dostos10@gmail.com>
2023-06-20 15:40:40 +0900

    DetectionActor wip -- requires to fix utexture2d crash

---
+++ unreal_engine/Source/BandExample/BandUIBase.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include "Blueprint/UserWidget.h"
+#include "BandBoundingBox.h"
+#include "BandUIBase.generated.h"
+
+UCLASS()
+class BANDEXAMPLE_API UBandUIBase : public UUserWidget
+{
+public:
+  GENERATED_BODY()
+
+  UPROPERTY(BlueprintReadWrite)
+  TArray<FBandBoundingBox> BoundingBoxes;
+};
---
+++ unreal_engine/Source/BandExample/DetectionActor.h
@@ -0,0 +1,57 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "BandLabel.h"
+#include "BandEnum.h"
+#include "BandModel.h"
+#include "CoreMinimal.h"
+#include "AndroidCameraComponent.h"
+#include "BandBoundingBox.h"
+#include "GameFramework/Actor.h"
+
+#include "DetectionActor.generated.h"
+
+class UImage;
+class UBandUIBase;
+
+UCLASS()
+class BANDEXAMPLE_API ADetectionActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ADetectionActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	void OnFrameAvailable(const UAndroidCameraFrame* Frame);
+	void OnTextureAvailable(UTexture2D* Texture);
+	void OnEndRequest(int32 JobId, EBandStatus Status);
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TSoftClassPtr<UBandUIBase> WidgetClass;
+	
+	UPROPERTY(BlueprintReadOnly, Transient)
+	UBandUIBase* Widget = nullptr;
+	UPROPERTY(BlueprintReadOnly, Transient)
+	UAndroidCameraComponent* AndroidCamera = nullptr;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Band")
+	UBandModel* DetectorModel = nullptr;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Band")
+	UBandLabel* Label = nullptr;
+
+private:
+	UImage* CameraImage = nullptr;
+
+	TArray<UBandTensor*> DetectorInputTensors;
+	TArray<UBandTensor*> DetectorOutputTensors;
+};
